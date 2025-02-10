aadb8774f1674f245f6593f1cbf49b602ea38d7e
YOUR NAMEcls <faiza.prism@gmail.com>
2025-02-10 18:15:59 +0500

    add loader

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,35 @@
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/router";
 import { LanguageContext } from "../pages/_app";
+import Loader from "./Loader/Loader";
 
 export default function Layout({ children }) {
   const { language } = useContext(LanguageContext);
-  console.log("language", language);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
   return (
     <div>
-      <main dir={language === "ar" ? "rtl" : "ltr"}>{children}</main>
+      {loading ? (
+        <Loader />
+      ) : (
+        <main dir={language === "ar" ? "rtl" : "ltr"}>{children}</main>
+      )}
     </div>
   );
 }
