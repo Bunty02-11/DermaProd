2a387328a2c02837acfcc26ac66af1a387027df9
YOUR NAMEcls <faiza.prism@gmail.com>
2025-02-24 15:47:28 +0500

    add sitemap

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://derma-prod.vercel.app",
-  generateRobotsTxt: true, // Generates robots.txt
-  //   exclude: ["/admin/*", "/dashboard/*"], // Exclude private pages
-};
